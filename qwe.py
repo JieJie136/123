ba2e4dd2a4c1e824f991f27b0c0add75da4bbddb
JieJie136 <156673498@qq.com>
2024-08-17 14:13:41 +0800

    222

---
+++ qwe.py
@@ -0,0 +1,25 @@
+
+money = 50
+print("钱包余额为:", money)
+ice = 5
+then = money - ice
+print("买完冰淇淋后钱包余额为:", then)
+cola = 10
+last = then - cola
+print("买完冰淇淋后和可乐后钱包余额为:", last)
+
+
+# qwe = str(11)
+# print(type(qwe), qwe)
+'''
+print("1 + 1 = ", 1 + 1)
+print("2 - 1 = ", 2 - 1)
+print("4 * 5 = ", 4 * 5)
+print("20 / 8 = ", 20 / 8)
+print("17 // 8 = ", 17 // 8)
+print("17 % 8 = ", 17 % 8)
+print("2 ** 3 = ", 2 ** 3)
+
+'''
+
+
